0c41bba0dea4fa093353c6baa392df62e303cde8
ElvisP23 <eperezsolutions.bot@gmail.com>
2023-04-04 10:07:04 -0400

    Se agrega carpeta 'services' y se añade la APIi /device/findAll exitosamente

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,14 +1,34 @@
 import React from 'react';
-//import Table from '../css/table.css';
 import Table from '../components/Table';
+import { useEffect, useState } from 'react';
+import { getAll } from '../services/DeviceService';
+
 
 const TablePage = () => {
+
+    // Usando useState
+    const [equipo, setEquipo] = useState([]);
+    console.log("holDespues del useState");
+
+     const obtenerEquipos = async () => {
+        console.log("hola dentro de obtener equipos");
+         setEquipo(await getAll());
+     }
+     console.log("holaGeneral2");
+
+     // Usnado useEffect
+     useEffect(() => {
+         console.log(equipo);
+         obtenerEquipos();
+     },[]);
+
+
+
     return (
-
-        <Table/>
-
-
-
+        <div>
+           
+            <Table equipo={equipo} />
+        </div>
 
 
 
