3034ec21a1a666fab5e24b207b2962f892034f56
ElvisP23 <eperezsolutions.bot@gmail.com>
2023-04-03 22:27:10 -0400

    Se crea Table.js, TablePage.js, table.css, index.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import './css/index.css'
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
 
