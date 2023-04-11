bb1c64c676a98e04ac62b89563753cbbde7ddff8
notvale <vgalvarezg1@gmail.com>
2023-04-11 12:21:23 -0400

    Se módifico registro page y se cambió por login page. se agregó componen login y css login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import RegistroPage from './pages/RegistroPage';
+import LoginPage from './pages/LoginPage';
 import TablePage from './pages/TablePage';
 import Dashboard from './pages/Dashboard';
 
@@ -12,7 +12,7 @@ const App = () => {
             <Routes>
 
                 <Route path='/' element={<LandingPage />}> </Route>
-                <Route path='/RegistroPage' element={<RegistroPage />}> </Route>
+                <Route path='/LoginPage' element={<LoginPage />}> </Route>
                 <Route path='/Dashboard' element={<Dashboard />}></Route>
                 <Route path='/TablePage' element={<TablePage />}> </Route>
                 {/* <Route path ='/FormularioPage' element={<FormularioPage/>}> </Route> */}
